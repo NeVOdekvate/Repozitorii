88856ed47eb44333841606f2b6450f75a8f7b4be
Alexey_Bogdanov <anestesiolog3333@gmail.com>
2026-07-15 14:36:35 +0300

    feat: add Lead class and change_name function

---
+++ lead.py
@@ -0,0 +1,19 @@
+class Lead:
+    """Лид (потенциальный клиент)."""
+
+    def __init__(self, name):
+        self.name = name
+
+NEW_NAME = "Иван"
+
+
+def change_name(lead):
+    lead.name = NEW_NAME
+
+
+if __name__ == "__main__":
+    lead1 = Lead("Пётр")
+    print("До изменения:", lead1.name)
+
+    change_name(lead1)
+    print("После изменения:", lead1.name)        
